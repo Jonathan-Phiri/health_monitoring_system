1dbc366fbebb5fff7046e6db85029fdef8eb9f14
Jonathan-Phiri <jonathanjaypumba@gmail.com>
2024-10-29 12:58:01 +0200

    h

--- health_monitoring_system/settings.py
+++ health_monitoring_system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'health_monitoring_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates't],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
